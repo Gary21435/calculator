0f34fd3397827339936e2fdd8f3856f29a61e471
gegi <35646675+Gary21435@users.noreply.github.com>
2025-03-10 00:14:48 -0700

    Implement +/- and AC

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ let num1 = null;
 let num2 = null;
 let twoNums = new Array(2);
 let operation = "";
-let done = false;
+let mustBeOperation = false;
 
 const operationArr = ["*", "/", "+", "-"];
 
@@ -60,6 +60,7 @@ function startOver() {
     num2 = null;
     operation = "";
     screenNum.textContent = 0;
+    mustBeOperation = false;
 }
 
 function doOperation() {
@@ -85,11 +86,16 @@ function doOperation() {
             ans = num1 / num2;
             break;
     }
+
+    startOver();
     screenNum.textContent = ans;
+    num1=ans;
+    mustBeOperation = true;
 }
 
 function handleClick(e) {
     let input = e.target.id;
+    console.log(input + " " + num1);
     if(input === "=") {
         doOperation();
     }
@@ -97,26 +103,40 @@ function handleClick(e) {
         startOver();
 
     let number = Number(input);
-    if(num1 == null && !number) {
+    let numCheck = number;
+    if(number === 0) numCheck = true; //0 is a number
+
+    if(num1 == null && !numCheck) {
         console.log("bro what");
         return;
     }
-    else if(number && operation == "") {
-        num1 = num1*10 + number;
+    else if(numCheck && operation == "") {
+        mustBeOperation ? num1 = number : num1 = num1*10 + number;        
         screenNum.textContent = num1;
         console.log(num1);
     }
-    else if(number && operation !== "") {
+    else if(numCheck && operation !== "") {
         num2 = num2*10 + number;
         screenNum.textContent += num2;
         console.log(num2);
     }
-    else if(!number && num1 != null && num2 == null) {
-        if(operationArr.includes(input)) {
-            operation = input;
-            screenNum.textContent += operation;
-        }
+    else if(!numCheck && num1 != null && num2 == null && operationArr.includes(input)) { // operation
+        operation = input;
+        screenNum.textContent += operation;
+        mustBeOperation = false;
         console.log("operation: ", operation);
+    }
+    else if(input === "+/-" && (num1 !== null || num2 !== null)) {
+        if(operation === "") {
+            num1 = -1 * num1;
+            console.log(num1);
+            screenNum.textContent = num1;
+        }
+        else {
+            num2 = -1 * num2;
+            console.log(num2);
+            screenNum.textContent = num1;
+        }
     }
 }
 
