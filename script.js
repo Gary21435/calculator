feccde79e8c12abce2d565fe08682adc6a1a44e5
gegi <35646675+Gary21435@users.noreply.github.com>
2025-03-10 15:46:56 -0700

    Fix a few more bugs

--- script.js
+++ script.js
@@ -54,7 +54,9 @@ for(let i = 0; i<buttonIDs.length; i++) {
 
 
 let num1 = null;
+let num1Str = "";
 let num2 = null;
+let num2Str = "";
 let twoNums = new Array(2);
 let operation = "";
 let mustBeOperation = false;
@@ -71,6 +73,8 @@ function startOver() {
     mustBeOperation = false;
     decimal1 = false;
     decimal2 = false;
+    num1Str = "";
+    num2Str = "";
 }
 
 function doOperation() {
@@ -101,9 +105,9 @@ function doOperation() {
         else ans = num1/100 * num2;
 
     startOver();
-    ans = Math.round(ans*100)/100; // Prevents 1.9999999999999999999
+    ans = Math.round(ans*10000000)/10000000; // Prevents 1.9999999999999999999
     screenNum.textContent = ans;
-    num1=ans;
+    num1Str = num1 = ans;
     mustBeOperation = true;
 }
 
@@ -112,9 +116,13 @@ function displayOperation() {
     let num2Text = num2;// = (num2 !== null) ? num2 : "";
     if(!decimal1) num1Text = (num1 !== null) ? num1 : "";
     else if(decimal1 && num1 === Math.floor(num1)) num1Text = (num1 !== null) ? num1+"." : ".";
+    console.log(num1);  
+    console.log(Math.floor(num1));
+    console.log(num1+".");  
+
     if(!decimal2) num2Text = (num2 !== null) ? num2 : "";
     else if(decimal2 && num2 === Math.floor(num2)) num2Text = (num2 !== null) ? num2+"." : ".";
-    screenNum.textContent = num1Text + operation + num2Text;
+    screenNum.textContent = num1Str + operation + num2Str;
 }
 
 function handleClick(e) {
@@ -123,11 +131,12 @@ function handleClick(e) {
     if(input === ".") {
         if(operation == "") {
             decimal1 = true;
+            num1Str += input;
             displayOperation();
-
         }
         else {
             decimal2 = true;
+            num2Str += input;
             displayOperation();
         }
     }
@@ -172,32 +181,43 @@ function handleClick(e) {
     else if(numCheck && operation == "") { // NUM1
         if(mustBeOperation) {
             startOver();
-            num1 = number; 
+            num1Str = num1 = number; 
+            displayOperation();
         }
-        else if(!decimal1) {
-            num1 = num1*10 + number;
-        } 
-        else {
-            let str1 = String(num1);
-            if(!str1.includes(".")) str1=str1.concat(".");
-            let dec = str1.length - str1.indexOf(".") - 1;
-            num1 += number/10**(dec+1);
-            num1 = Math.round(num1*100)/100;
-        }
-        screenNum.textContent = num1;
+        // else if(!decimal1) {
+        //     num1 = num1*10 + number;
+        // } 
+        // else {
+        //     let str1 = String(num1);
+        //     // let dec = str1.length - str1.indexOf(".") - 1;
+        //     // num1 += number/10**(dec+1);
+        //     // num1 = Math.round(num1*100)/100;
+        //     // if(!str1.includes(".")) {
+        //     //     str1 += ".";
+        //     //     num1Str = str1;
+        //     // }
+            
+        // }
+        num1Str += String(number);
+        num1 = Number(num1Str);
+        screenNum.textContent = num1Str;
     }
     else if(numCheck && operation !== "") { // NUM2
-        if(!decimal2) {
-            num2 = num2*10 + number;
-        } 
-        else {
-            let str2 = String(num2);
-            if(!str2.includes(".")) str2=str2.concat(".");
-            let dec = str2.length - str2.indexOf(".") - 1;
-            num2 += number/10**(dec+1);
-            num2 = Math.round(num2*100)/100;
-        }
-        screenNum.textContent = num1 + operation + num2;
+        // if(!decimal2) {
+        //     num2 = num2*10 + number;
+        // } 
+        // else {
+        //     let str2 = String(num2);
+        //     if(!str2.includes(".")) str2=str2.concat(".");
+        //     let dec = str2.length - str2.indexOf(".") - 1;
+        //     num2 += number/10**(dec+1);
+        //     num2 = Math.round(num2*10000000)/10000000;
+        // }
+        num2Str += String(number);
+        num2 = Number(num2Str);
+        screenNum.textContent = num2Str;
+        displayOperation();
+        // screenNum.textContent = num1 + operation + num2;
     }
     else if(!numCheck && num1 != null && num2 == null && operationArr.includes(input)) { // operation
         if(operation === input) return;
